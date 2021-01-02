7f5e240bc1f8828f8c342d93e2374ba6e8056799
itDarashukD <itdarashukd@gmail.com>
2021-01-02 15:07:05 +0300

    создание файла .property и подтягивание от туда значений в поля

--- src/main/java/org/example/springIntroduction/Person.java
+++ src/main/java/org/example/springIntroduction/Person.java
@@ -2,6 +2,8 @@ package org.example.springIntroduction;
 
 public class Person {
     private Pet pet;
+    private String name;
+    private int age;
 
 
     public Person() {
@@ -17,5 +19,23 @@ public class Person {
     public void callYourPet(){
         System.out.println("hello my dear pet!");
         pet.say();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println("присваиваем имя" );
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("присваиваем возраст" );
+        this.age = age;
     }
 }
--- src/main/java/org/example/springIntroduction/Test3.java
+++ src/main/java/org/example/springIntroduction/Test3.java
@@ -10,6 +10,10 @@ public class Test3 {
         Person person = context.getBean("myPerson",Person.class);
         person.callYourPet();
 
+        System.out.println(person.getAge());
+        System.out.println(person.getName());
+
+
         context.close();
     }
 }

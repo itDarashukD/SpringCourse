c6f68e6df1da5b5a8eb10ad2c14baa2ebfb72e78
itDarashukD <itdarashukd@gmail.com>
2021-01-07 12:24:55 +0300

    Hibernate #1

---
+++ src/main/java/org/hibernateTest/Entity/App.java
@@ -0,0 +1,30 @@
+package org.hibernateTest.Entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class App {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+
+
+            Session session = factory.getCurrentSession();
+
+            Employee employee = new Employee("Dzmitry", "Darashuk", "IT", 3500);
+
+            session.beginTransaction();
+            session.save(employee);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+
+    }
+}
